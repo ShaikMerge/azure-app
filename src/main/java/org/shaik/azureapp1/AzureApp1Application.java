13503f16c37c79e2443e2fe5bf0ffd025a5e7f1d
shaik shajahan <shajahanshaik0987@gmail.com>
2026-06-25 20:18:08 +0530

    Inital commit

---
+++ src/main/java/org/shaik/azureapp1/AzureApp1Application.java
@@ -0,0 +1,13 @@
+package org.shaik.azureapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureApp1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzureApp1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/shaik/azureapp1/AzureApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shaik.azureapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureApp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
