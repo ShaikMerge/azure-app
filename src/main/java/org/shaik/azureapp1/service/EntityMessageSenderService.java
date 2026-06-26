940917facfe97ac4c79e351df7abee48648ef9d0
shaik shajahan <shajahanshaik0987@gmail.com>
2026-06-26 20:36:31 +0530

    Update code

--- src/main/java/org/shaik/azureapp1/service/EntityMessageSenderService.java
+++ src/main/java/org/shaik/azureapp1/service/EntityMessageSenderService.java
@@ -3,6 +3,7 @@ package org.shaik.azureapp1.service;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.spring.messaging.servicebus.core.ServiceBusTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
@@ -11,15 +12,15 @@ public class EntityMessageSenderService {
 
     @Autowired
     private ServiceBusTemplate serviceBusTemplate;
-    @Autowired
-    private SecretClient secretClient;
+    @Value("${queue.name}")
+    private String getQueueName;
 
     private static final String QUEUE_NAME = "shaik-exchange-dev";
 
 
     public void sendMessageToServiceBus(String message) {
         serviceBusTemplate.send(QUEUE_NAME, MessageBuilder.withPayload(message).build());
-        System.out.println("QUEUE-NAME FROM SECRETS: " + secretClient.getSecret("queue-name").getValue());
+        System.out.println("QUEUE-NAME FROM SECRETS: " + getQueueName);
         System.out.println("Sent message to Service Bus: " + message);
     }
 }
