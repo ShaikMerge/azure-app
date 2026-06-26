0f828e17b7d3f74dcc33d8484f52efde6fb08206
shaik shajahan <shajahanshaik0987@gmail.com>
2026-06-26 20:28:01 +0530

    Update code

--- src/main/java/org/shaik/azureapp1/configuration/SecretClientConfiguration.java
+++ src/main/java/org/shaik/azureapp1/configuration/SecretClientConfiguration.java
@@ -1,19 +1,19 @@
-//package org.shaik.azureapp1.configuration;
-//
-//import com.azure.identity.DefaultAzureCredentialBuilder;
-//import com.azure.security.keyvault.secrets.SecretClient;
-//import com.azure.security.keyvault.secrets.SecretClientBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class SecretClientConfiguration {
-//
-//    @Bean
-//    public SecretClient createSecretClient() {
-//        return new SecretClientBuilder()
-//                .vaultUrl("https://shaik-exchange-key-valut.vault.azure.net/")
-//                .credential(new DefaultAzureCredentialBuilder().build())
-//                .buildClient();
-//    }
-//}
+package org.shaik.azureapp1.configuration;
+
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecretClientConfiguration {
+
+    @Bean
+    public SecretClient createSecretClient() {
+        return new SecretClientBuilder()
+                .vaultUrl("https://shaik-exchange-key-valut.vault.azure.net/")
+                .credential(new DefaultAzureCredentialBuilder().build())
+                .buildClient();
+    }
+}
